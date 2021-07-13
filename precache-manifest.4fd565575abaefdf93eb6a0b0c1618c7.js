09cf2e6c18c78714210d4d309cb3efb099b046a1
karimihab2022 <kareemihabsalah@gmail.com>
2021-07-13 20:00:22 +0200

    Updates

---
+++ precache-manifest.4fd565575abaefdf93eb6a0b0c1618c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "85b4fd121ffdc5760f11",
+    "url": "/MyReads/static/css/main.95e771d1.chunk.css"
+  },
+  {
+    "revision": "85b4fd121ffdc5760f11",
+    "url": "/MyReads/static/js/main.85b4fd12.chunk.js"
+  },
+  {
+    "revision": "cd8e0a62429806be9581",
+    "url": "/MyReads/static/js/1.cd8e0a62.chunk.js"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/MyReads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/MyReads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/MyReads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "196226807f979027ae20f0aee314b776",
+    "url": "/MyReads/index.html"
+  }
+];
